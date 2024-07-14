b2de4b0bfb6e8f65acaf889b389d78869f683195
waseem tasawar <waseemtasawar97@gmail.com>
2024-07-14 15:42:27 +0500

    Updating all UI with context API

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,12 +1,11 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
-import Cart from './Cart';
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+import Cart from "./Cart";
+// import { CartContext } from "../store/Shopping-cart-context";
 
-const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
-  ref
-) {
+const CartModal = forwardRef(function Modal({ title, actions }, ref) {
   const dialog = useRef();
+  // const { items, updateItemQuantity } = useContext(CartContext);
 
   useImperativeHandle(ref, () => {
     return {
@@ -19,12 +18,12 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 });
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,20 @@
-export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
-}) {
+import { useContext } from "react";
+
+import { CartContext } from "../store/Shopping-cart-context";
+
+export default function Product({ id, image, title, price, description }) {
+  const { addItemsToCart } = useContext(CartContext);
   return (
     <article className="product">
       <img src={image} alt={title} />
       <div className="product-content">
         <div>
           <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
+          <p className="product-price">${price}</p>
           <p>{description}</p>
         </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+        <p className="product-actions">
+          <button onClick={() => addItemsToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
